a1047ada64634949dd4c419990d3829f316c58ce
yonaathlan <yonaathlan1313@gmail.com>
2026-06-18 21:29:30 +0300

    fix: hardcode public firebase config to fix missing env error

--- frontend/src/services/firebase/config.js
+++ frontend/src/services/firebase/config.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyDiNaKN9JkKIphBdy9fhZPdbbDdxu3_m6I",
+    authDomain: "employemnt-autority.firebaseapp.com",
+    projectId: "employemnt-autority",
+    storageBucket: "employemnt-autority.firebasestorage.app",
+    messagingSenderId: "375963334236",
+    appId: "1:375963334236:web:75d179541e733071821899"
 };
 
 import { getFunctions } from "firebase/functions";
